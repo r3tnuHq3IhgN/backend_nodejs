dddbcbe0d1729fec88039e7fe03435642a17b06d
nghiepvh <nghiephunter.uet.dev@gmail.com>
2024-10-23 16:58:27 +0700

    authentication and logout

--- src/auth/authUltils.js
+++ src/auth/authUltils.js
@@ -1,10 +1,24 @@
 'use strict'
 
 const JWT = require('jsonwebtoken');
+const { asyncHandler } = require('../helpers/asyncHandler');
+const { AuthFailureError, NotFoundError } = require('../core/error.response');
+const { findKeyToken } = require('../services/keytoken.service');
+
+const HEADER = {
+    API_KEY: 'x-api-key',
+    AUTHORIZATION: 'authorization',
+    CLIENT_ID: 'x-client-id'
+
+
+}
+
 const createTokenPair = async (payload, publicKey, privateKey) => {
     try {
         console.log('publicKey::', publicKey);
         console.log('privateKey::', privateKey);
+
+        // create accessToken and refreshToken
         const accessToken = await JWT.sign(payload, privateKey, {
             algorithm: 'RS256',
             expiresIn: '1d' 
@@ -14,6 +28,7 @@ const createTokenPair = async (payload, publicKey, privateKey) => {
             expiresIn: '1d' 
         });
 
+        // verify accessToken with publicKey
         JWT.verify(accessToken, publicKey, (err, decode) => {
             if (err) {
                 console.log('err verify::', err);
@@ -26,11 +41,38 @@ const createTokenPair = async (payload, publicKey, privateKey) => {
     } catch (error) {
         throw error;
     }
-
-
-
 }
 
+
+const authentication =  asyncHandler( async (req, res, next) => {
+    // get user_id from header
+    const user_id = req.headers[HEADER.CLIENT_ID];
+    if (!user_id) throw new AuthFailureError('Invalid request: User not found');
+
+    // find key token by user_id
+    const keyStore = await findKeyToken(user_id);
+    if (!keyStore) throw new NotFoundError('Key not found');
+
+    // get accessToken from header
+    const accessToken = req.headers[HEADER.AUTHORIZATION];
+    if (!accessToken) throw new AuthFailureError('Invalid request: Token not found');
+
+    try {
+        // verify accessToken with publicKey and get decodeUser
+        const decodeUser = JWT.verify(accessToken, keyStore.publicKey);
+        if (!decodeUser) throw new AuthFailureError('Invalid token');
+
+        // check user_id from header and decodeUser
+        if(user_id !== decodeUser.userId) throw new AuthFailureError('Invalid user');
+        req.keyStore = keyStore;
+        console.log('decodeUser::', decodeUser);
+        next();
+    } catch (error) {
+        throw new AuthFailureError('Invalid token');
+    }
+});
+
 module.exports = {
-    createTokenPair
+    createTokenPair,
+    authentication
 }
--- src/routes/access/index.js
+++ src/routes/access/index.js
@@ -3,10 +3,18 @@
 const express = require('express');
 const router = express.Router();
 const accessController = require('../../controllers/access.controler.js');
-const { asyncHandler } = require('../../auth/checkAuth.js');
+const { asyncHandler } = require('../../helpers/asyncHandler');
+const { authentication } = require('../../auth/authUltils.js');
 
 // sign up
 router.post('/shop/signup', asyncHandler(accessController.signUp));
 router.post('/shop/login', asyncHandler(accessController.signIn));
+
+//authentication
+router.use(authentication);
+
+// log out
+router.post('/shop/logout', asyncHandler(accessController.logOut));
+
 
 module.exports = router;
